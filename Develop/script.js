d55a0b14a072e4ad534939a6d348b7dacec7cec1
Brianna Ligon <desmondsaelee@hotmail.com>
2022-11-27 13:06:30 -0800

    added variables and if statements in the order I want them to appear and added the strings from the global scope variables. Also added notes to some of the lines.

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// Added my variables. The things I need to keep track of.
+// Added my variables at a global scope. The things I need to keep track of.
 var generateBtn = document.querySelector("#generate");
 var x = 8;
 var y = 128;
@@ -9,22 +9,39 @@ var numbers = "0123456789";
 var userInput = "";
 var userPassword = "";
 
-// Created a function to prompt for user input, user confirmation, and to alert the user for the criteria needed to generate random password.
+// Created variables within the function.
 function generatePassword() {
   var userInput = "";
   var userPassword = "";
   var passwordprompt = prompt("Enter desired password length (between 8- 128 characters)");
-  var confirmLowerCase = confirm ("Include lowercase letters?")
 
+// Added variables and if statements in the order I want them to appear and added the strings from the global scope variables.
   if ( (passwordprompt < x || passwordprompt > y) )  {
     alert ("Please input desired password length (between 8- 128 characters)");
     return;
   }
 
+  var confirmLowerCase = confirm ("Include lowercase letters?")
   if (confirmLowerCase) {
     userInput = userInput.concat(lowercase)
   }
 
+  var confirmUpperCase = confirm ("Include uppercase letters?")
+  if (confirmUpperCase) {
+    userInput = userInput.concat(uppercase)
+  }
+
+  var confirmNumber = confirm ("Include numbers?")
+  if (confirmNumber) {
+    userInput = userInput.concat(numbers)
+  }
+
+  var confirmSpecialCharacters = confirm ("Include special characters?")
+  if (confirmSpecialCharacters) {
+    userInput = userInput.concat(specialCharacters)
+  }
+
+  console.log("user input = " + userInput);
   
 
 
